13080adea37bb8d6c4015093da5a4843144adcbd
guskirb <anguskirby1998@gmail.com>
2024-10-19 11:13:05 +0100

    Add handle save hotspot logic

--- src/components/Modals/EditHotspot/EditHotspotModal.js
+++ src/components/Modals/EditHotspot/EditHotspotModal.js
@@ -1,18 +1,46 @@
 import "./EditHotspotModal.scss";
 
+import { useEffect, useState } from "react";
+
 import CloseButton from "../../Buttons/Close/CloseButton"
 import Modal from "../Modal/Modal"
-import { useEffect } from "react";
+import { useCurrentVariant } from "../../../contexts/useCurrentVariant";
+import { useCurrentProduct } from "../../../contexts/useCurrentProduct";
+import { useConfig } from "../../../contexts/useConfig";
 
 export const EditHotspotModal = (props) => {
+    const [value, setValue] = useState(props.hotspot.content ? props.hotspot.content : "");
+    const [currentVariant, setCurrentVariant] = useCurrentVariant();
+    const [config, setConfig] = useConfig();
+    const [currentProduct] = useCurrentProduct();
 
-    useEffect(() => {
-        console.log(props.hotspot)
-    }, [props])
+    function handleSave(e) {
+        e.preventDefault();
+
+        let currVar = {
+            ...currentVariant, hotspots: currentVariant.hotspots.map((hotspot) => hotspot.id !== props.hotspot.id ? hotspot : {
+                ...hotspot,
+                content: value,
+            })
+        };
+
+        setCurrentVariant(currVar);
+        setConfig({
+            ...config,
+            products: config.products.map((product) => product.uuid !== currentProduct.uuid ? product : {
+                ...currentProduct, variants: currentProduct.variants.map((variant) => variant.uuid !== currentVariant.uuid ? variant : currVar)
+            })
+        })
+        
+        props.updateModalState(false);
+    }
 
     return (
         <Modal canShow={props.canShow} id="edit-hotspot-modal">
-            <CloseButton closeModal={() => props.updateModalState(false)} />
+            <CloseButton closeModal={() => {
+                setValue(props.hotspot.content);
+                props.updateModalState(false);
+            }} />
             <h2 className="edit-hotspot-modal__title">
                 Edit Hotspot
             </h2>
@@ -20,7 +48,12 @@ export const EditHotspotModal = (props) => {
             <div className="edit-hotspot-modal__list">
                 <form className="edit-hotspot-modal__form">
                     <label>Hotspot Name</label>
-                    <input className="edit-hotspot-modal__input" type="text" value={props.hotspot.content} />
+                    <input
+                        className="edit-hotspot-modal__input"
+                        type="text" value={value}
+                        onChange={e => setValue(e.target.value)}
+                    />
+                    <button onClick={handleSave}>Save</button>
                 </form>
             </div>
         </Modal>
