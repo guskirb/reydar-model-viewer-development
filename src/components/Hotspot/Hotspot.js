ab62fe877bf99a640a736080271b817243ea454f
guskirb <anguskirby1998@gmail.com>
2024-10-19 10:28:12 +0100

    Add edit hotspot modal on hotspot click

--- src/components/Hotspot/Hotspot.js
+++ src/components/Hotspot/Hotspot.js
@@ -1,23 +1,36 @@
 import "./Hotspot.scss";
+
+import { useState } from "react";
+
 import LabelText from "../Labels/Text/LabelText";
+import { EditHotspotModal } from "../Modals/EditHotspot/EditHotspotModal";
 
 export default function Hotspot({ hotspot }) {
    const position = `${hotspot.position.x} ${hotspot.position.y} ${hotspot.position.z}`;
    const normal = `${hotspot.normal.x} ${hotspot.normal.y} ${hotspot.normal.z}`;
+   const [showModal, setShowModal] = useState();
+
+   function handleClick(e) {
+      setShowModal(true);
+   }
 
    return (
-      <button
-         className="hotspot"
-         data-normal={normal}
-         data-position={position}
-         slot={`hotspot-${hotspot.id}`}
-         data-visibility-attribute="visible"
-      >
-         {hotspot.content && (
-            <div className="hotspot__label">
-               <LabelText content={hotspot.content} />
-            </div>
-         )}
-      </button>
+      <>
+         <button
+            className="hotspot"
+            data-normal={normal}
+            data-position={position}
+            slot={`hotspot-${hotspot.id}`}
+            data-visibility-attribute="visible"
+            onClick={handleClick}
+         >
+            {hotspot.content && (
+               <div className="hotspot__label">
+                  <LabelText content={hotspot.content} />
+               </div>
+            )}
+         </button>
+         <EditHotspotModal canShow={showModal} updateModalState={setShowModal} hotspot={hotspot} />
+      </>
    );
 }
--- src/components/Modals/EditHotspot/EditHotspotModal.js
+++ src/components/Modals/EditHotspot/EditHotspotModal.js
@@ -1,29 +1,27 @@
 import "./EditHotspotModal.scss";
 
-import { useEffect } from "react"
-
 import CloseButton from "../../Buttons/Close/CloseButton"
 import Modal from "../Modal/Modal"
+import { useEffect } from "react";
 
 export const EditHotspotModal = (props) => {
 
     useEffect(() => {
-        console.log(props.currentVariant);
-    }, [props.currentVariant]);
+        console.log(props.hotspot)
+    }, [props])
 
     return (
         <Modal canShow={props.canShow} id="edit-hotspot-modal">
             <CloseButton closeModal={() => props.updateModalState(false)} />
             <h2 className="edit-hotspot-modal__title">
-                Edit Hotspots
+                Edit Hotspot
             </h2>
 
             <div className="edit-hotspot-modal__list">
-                {props.currentVariant.hotspots ? props.currentVariant.hotspots.map((hotspot, index) => (
-                    <div key={index}>
-                        <h3>{hotspot.content}</h3>
-                    </div>
-                )) : null}
+                <form>
+                    <label />
+                    <input type="text" value={props.hotspot.content} />
+                </form>
             </div>
         </Modal>
     )
