06fe27fc6d418a9c0145832a914b8e1323407558
guskirb <anguskirby1998@gmail.com>
2024-10-19 10:06:43 +0100

    Add icon

--- src/components/Buttons/AddHotspot/AddHotspot.js
+++ src/components/Buttons/AddHotspot/AddHotspot.js
@@ -1,6 +1,8 @@
 import { useAction } from "../../../contexts/useAction"
 import { useConfig } from "../../../contexts/useConfig";
+
 import ActionButtonViewerState from "../Action/ActionButtonViewerState";
+import AddIcon from "../../../assets/icons/add.png";
 
 export const AddHotspot = () => {
     const [config] = useConfig();
@@ -11,6 +13,7 @@ export const AddHotspot = () => {
             <ActionButtonViewerState
                 label="Add"
                 icon="fa-light fa-eye"
+                iconImg={AddIcon}
                 action="add"
                 active={action.add}
                 handleStateButtonClicked={setAction}
