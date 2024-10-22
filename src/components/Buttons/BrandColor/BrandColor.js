1dfbed7cce5e7860c619bb794ba272531f9d9c98
guskirb <anguskirby1998@gmail.com>
2024-10-22 10:37:14 +0100

    Fix eslint errors

--- src/components/Buttons/BrandColor/BrandColor.js
+++ src/components/Buttons/BrandColor/BrandColor.js
@@ -4,14 +4,14 @@ import { useConfig } from "../../../contexts/useConfig";
 import { ColorButton } from "../../Buttons/Color/ColorButton";
 
 export const BrandColor = () => {
-    const [config, setConfig] = useConfig();
+    const [config] = useConfig();
     const [color, setColor] = useState(config["brand-color"]);
 
     useEffect(() => {
         const documentRoot = document.documentElement;
         config["brand-color"] = color;
         documentRoot.style.setProperty("--brand-color", color);
-    }, [color])
+    }, [color, config])
 
     return (
         <>
--- src/components/Modals/EditHotspot/EditHotspotModal.js
+++ src/components/Modals/EditHotspot/EditHotspotModal.js
@@ -30,6 +30,7 @@ export const EditHotspotModal = (props) => {
         documentRoot.style.setProperty("--hotspot-color", color);
 
         setCurrentVariant(currVar);
+        console.log(config);
         setConfig({
             ...config,
             products: config.products.map((product) => product.uuid !== currentProduct.uuid ? product : {
